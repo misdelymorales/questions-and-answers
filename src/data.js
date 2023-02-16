c79e6895547dea59b0eba02b282388883c85d649
Misdely Morales <misdely.m@gmail.com>
2023-02-16 20:25:10 -0300

    creando proyecto de preguntas y respuestas

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: "¿Tengo que permitir el uso de cookies?",
+    info: "Unicornio vinilo poutine brooklyn, siguiente nivel comercio directo islandia. Chamán cobre taza iglesia-clave libro para colorear, lo que sea poutine normcore fixie cred kickstarter post-ironic street art.",
+  },
+  {
+    id: 2,
+    title: "¿Cómo cambio la contraseña de Mi página?",
+    info: "Libro para colorear fotomatón de forraje aburguesado lumbersexual. Migas chillwave poutine synth shoreditch, esmalte pin thundercats moda hacha techo fiesta polaroid chartreuse.",
+  },
+  {
+    id: 3,
+    title: "¿Qué es BankID?",
+    info: "Esmalte pin fam sostenible despertó cualquier venmo. Auténtico asimétrico ponle un pájaro, lumbersexual carbón activado parentesco banjo cred sartorial en escabeche.",
+  },
+  {
+    id: 4,
+    title: "¿De quién es el número de nacimiento que puedo usar?",
+    info: "'Bombilla Edison comercio directo gentrify barba lo-fi seitán techo sostenible fiesta franzen ocupar calamar. Suculentas de cronut de Knausgaard, lyft shabby chic listo para usar. Taza de cobre meh vegan gentrify.",
+  },
+  {
+    id: 5,
+    title: "¿Cuándo recibo una contraseña ordenada por letra?",
+    info: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave chaleco cobre mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitán ",
+  },
+];
+export default questions;
